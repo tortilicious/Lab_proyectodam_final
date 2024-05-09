864bdcfc4f4f1b54e0cf70656a8777aae0797c8a
tortilicious <miggg89@gmail.com>
2024-05-09 02:40:44 +0200

    user2 implementa pruebas

--- src/testing/TestCalculadora.java
+++ src/testing/TestCalculadora.java
@@ -1,8 +1,18 @@
 package testing;
 
+import dao.CalculadoraDao;
+import dao.CalculadoraDaoImpl;
+
 public class TestCalculadora {
 
     public static void main(String[] args) {
+        CalculadoraDao calculadora = new CalculadoraDaoImpl();
+
+        System.out.println(calculadora.sumar(20, 5));
+        System.out.println(calculadora.restar(20, 5));
+        System.out.println(calculadora.multiplicar(20, 5));
+        System.out.println(calculadora.dividir(20, 5));
+        System.out.println(calculadora.resto(20, 5));
 
     }
 }

4407730ffb0c0958d02c01c2a72cc2e6b42097eb
tortilicious <miggg89@gmail.com>
2024-05-09 02:29:12 +0200

    user1 implementacion la logica calculadora

--- src/dao/CalculadoraDaoImpl.java
+++ src/dao/CalculadoraDaoImpl.java
@@ -4,26 +4,26 @@ public class CalculadoraDaoImpl implements CalculadoraDao{
 
     @Override
     public int sumar(int op1, int op2) {
-        return 0;
+        return op1 + op2;
     }
 
     @Override
     public int restar(int op1, int op2) {
-        return 0;
+        return op1 - op2;
     }
 
     @Override
     public int multiplicar(int op1, int op2) {
-        return 0;
+        return op1 * op2;
     }
 
     @Override
     public int dividir(int op1, int op2) {
-        return 0;
+        return op1 / op2;
     }
 
     @Override
     public int resto(int op1, int op2) {
-        return 0;
+        return op1%op2;
     }
 }
